e1800f1c43462a5a55c8a1681716eea17f56fa22
tkdqh57 <tkdqh57@naver.com>
2025-12-19 18:00:01 +0900

    1일차

--- 1일차/app.py
+++ 1일차/app.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     users = [
+        {"username": "admin", "name": "Sangbo"},
         {"username": "traveler", "name": "Alex"},
         {"username": "photograpger", "name": "Sam"},
         {"username": "gourmet", "name": "Chris"}
